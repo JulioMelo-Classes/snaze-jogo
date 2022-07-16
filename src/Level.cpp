d9fabed82ee1747eb4a27f324dfad586f07b4bbf
Danrley Lima <danrleylac@gmail.com>
2022-07-16 18:03:22 -0300

    Corrigindo sorteio em posições não válidas

--- src/Level.cpp
+++ src/Level.cpp
@@ -41,7 +41,7 @@ void Level::colocar_comida() {
     while (true) {
         int linha_comida = linha(drl);
         int coluna_comida = coluna(drc);
-        if ((get_maze_element(linha_comida, coluna_comida) != '#') || (get_maze_element(linha_comida, coluna_comida) != '.')) {
+        if ((get_maze_element(linha_comida, coluna_comida) != '#') && (get_maze_element(linha_comida, coluna_comida) != '.')) {
             // m_pos_comida = make_pair(m_init_linha, m_init_coluna+1);
             m_pos_comida = make_pair(linha_comida, coluna_comida);
             // m_maze[m_init_linha][m_init_coluna+1] = 'F';
